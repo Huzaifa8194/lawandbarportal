cb7f49a204e9c1672c52ef4a980c2df0d7fbc7fd
Huzaifa8194 <huzaifa8195@gmail.com>
2026-04-03 20:13:44 +0500

    2nd update

--- app/api/admin/students/route.ts
+++ app/api/admin/students/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { verifyAdminRequest } from "../_lib/auth";
 import { adminDb } from "@/lib/firebase-admin";
 import { getSqeBundlePurchaserEmailSet } from "../_lib/sqe-bundle-purchase";
-
+//random comment
 export async function GET(request: NextRequest) {
   try {
     await verifyAdminRequest(request);
