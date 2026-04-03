5208f1056942e044d4ae7782f5c970db9c86ada4
Huzaifa8194 <huzaifa8195@gmail.com>
2026-04-03 18:34:42 +0500

    2nd update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import "@cyntler/react-doc-viewer/dist/index.css";
 import { AuthProvider } from "./context/auth-context";
 
 const geistSans = Geist({
